0ceed0e975ac258d63684c25e812df22488f9d31
atoc12 <octaviosdf1@gmail.com>
2023-10-24 20:28:39 -0300

    update pagev2

--- client/src/config/var.js
+++ client/src/config/var.js
@@ -1,5 +1,5 @@
 export const appVar ={
-    ip:"http://localhost",
-    // ip:"https://4d59-2800-810-4fe-14a9-a116-b152-21d6-bde6.ngrok-free.app",
+    // ip:"http://localhost",
+    ip:"http://52.67.110.73",
     port:"80"
 }
