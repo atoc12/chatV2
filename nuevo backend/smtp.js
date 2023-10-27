fa78d2fd9618054d608f34e8bf087c16ae9ebf13
atoc12 <octaviosdf1@gmail.com>
2023-10-27 00:42:35 -0300

    update1123

--- nuevo backend/smtp.js
+++ nuevo backend/smtp.js
@@ -1,41 +1,60 @@
-const SMTPServer = require('smtp-server').SMTPServer;
-const tls = require('tls');
+const nodemailer = require('nodemailer');
 const fs = require('fs');
-const privateKeyPath = '/etc/letsencrypt/live/essec.ddns.net/privkey.pem'; // Ruta a tu clave privada
-const certificatePath = '/etc/letsencrypt/live/essec.ddns.net/fullchain.pem'; // Ruta a tu certificado completo
+const SMTPServer = require('smtp-server').SMTPServer;
 
-const enviarMail =async ()=>{
-    const config = {
-        host:"essec.ddns.net",
-        port:465,
-        auth:{
-            user:"octaviosdf1@gmail.com",
-            pass:"vqfh fqpv ekzg kmkb"
-        }
-    }
-    const mensaje = {
-        from :'octaviosdf1@gmail.com',
-        to:"octaviosdf2@gmail.com",
-        subject:"Correo de pruebas",
-        text:"hola mundo"
-    }
-    const transport = nodeMail.createTransport(config);
-    const info = await transport.sendMail(mensaje);
-    console.log(info);
-  }
+// Rutas a tus claves y certificados SSL
+const privateKeyPath = '/etc/letsencrypt/live/essec.ddns.net/privkey.pem';
+const certificatePath = '/etc/letsencrypt/live/essec.ddns.net/fullchain.pem';
 
+// Configuración del servidor SMTP
 const server = new SMTPServer({
-  secure: true, // Habilita la seguridad TLS
-  key: fs.readFileSync(privateKeyPath), // Ruta a tu clave privada
-  cert: fs.readFileSync(certificatePath), // Ruta a tu certificado SSL
+  secure: true, // Habilita SSL/TLS
+  key: fs.readFileSync(privateKeyPath), // Clave privada SSL
+  cert: fs.readFileSync(certificatePath), // Certificado SSL
   onAuth(auth, session, callback) {
-    // Lógica de autenticación
-  },
-  onData(stream, session, callback) {
-    enviarMail();    
+    // Verifica las credenciales del usuario aquí
+    const username = 'tu_usuario';
+    const password = 'tu_contraseña';
+    if (auth.username === username && auth.password === password) {
+      return callback(null, { user: 'user' });
+    } else {
+      return callback(new Error('Autenticación fallida'));
+    }
   },
 });
 
 server.listen(465, () => {
-  console.log('Servidor SMTP seguro escuchando en el puerto 465');
+  console.log('Servidor SMTP escuchando en el puerto 465 (SSL/TLS)');
+});
+
+// Configuración del cliente de correo electrónico (nodemailer)
+const transporter = nodemailer.createTransport({
+  host: 'essec.ddns.net', // Servidor SMTP
+  port: 465, // Puerto seguro para SSL/TLS
+  secure: true, // Habilitar SSL/TLS
+  auth: {
+    user: 'tu_usuario', // Usuario SMTP
+    pass: 'tu_contraseña', // Contraseña SMTP
+  },
+  tls: {
+    // Configuración de TLS
+    ca: [fs.readFileSync(certificatePath)],
+  },
+});
+
+// Detalles del mensaje
+const mailOptions = {
+  from: 'tucorreo@essec.ddns.net',
+  to: 'octaviosdf1@gmail.com',
+  subject: 'Asunto del correo',
+  text: 'Cuerpo del correo',
+};
+
+// Enviar el correo electrónico
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.error('Error al enviar el correo:', error);
+  } else {
+    console.log('Correo enviado:', info.response);
+  }
 });
